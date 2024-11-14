579e2c8398731f548842acfa2f0319b75eac6c99
Ankit Pande <arprocks1583@gmail.com>
2024-11-14 19:47:38 +0530

    '2'

--- blog_1132230145/settings.py
+++ blog_1132230145/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hxe&g2tzj**bhzpr8yu1kv)p83$pqnnb2^ebeudxeg#f!xou+x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
